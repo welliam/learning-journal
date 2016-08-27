f1c139509d2a17e5590d488c7791b88167e63f79
Justin Lange <well1912@gmail.com>
2016-08-27 14:26:55 -0700

    Add tests for EntryModel
    comment out view testing (until it's useful)

--- learning_journal/tests/tests.py
+++ learning_journal/tests/tests.py
@@ -4,12 +4,19 @@ import transaction
 from pyramid import testing
 
 from ..models import (
-    MyModel,
+    EntryModel,
     get_engine,
     get_session_factory,
     get_tm_session,
 )
 from ..models.meta import Base
+
+TITLES = [
+    'Day 8',
+    'Day 9',
+    'Day -3',
+    'Day Foo'
+]
 
 
 @pytest.fixture(scope="session")
@@ -41,3 +48,43 @@ def new_session(sqlengine, request):
 
     request.addfinalizer(teardown)
     return session
+
+
+def test_model_gets_added(new_session):
+    assert len(new_session.query(EntryModel).all()) == 0
+    model = EntryModel(title="a", body="b", date="never")
+    new_session.add(model)
+    new_session.flush()
+    assert len(new_session.query(EntryModel).all()) == 1
+
+
+def test_model_saves_title(new_session):
+    assert len(new_session.query(EntryModel).all()) == 0
+    model = EntryModel(title="a", body="b", date="never")
+    new_session.add(model)
+    new_session.flush()
+    assert new_session.query(EntryModel).one().title == 'a'
+
+
+@pytest.mark.parametrize('titles', TITLES)
+def test_model_saves_titles(new_session, titles):
+    for title in titles:
+        new_session.add(EntryModel(title=title, body="b", date="never"))
+    new_session.flush()
+    results = map(lambda e: e.title, new_session.query(EntryModel).all())
+    assert set(titles) == set(results)
+
+
+# def dummy_http_request(new_session):
+#     return testing.DummyRequest()
+#
+#
+# def test_my_view(new_session):
+#     from ..views.default import my_view
+#
+#     new_session.add(EntryModel(name="one", value=1))
+#     new_session.flush()
+#
+#     http_request = dummy_request(new_session)
+#     result = my_view(http_request)
+#     assert result["one"].name == "one"
