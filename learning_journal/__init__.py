b43970d07378ef1ea73fb9b923802e7a89a51f02
Justin Lange <well1912@gmail.com>
2016-08-29 00:45:59 -0700

    Add DATABASE_URL to settings

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -6,6 +6,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     # add static view
+    settings['sqlalchemy.url'] = os.environ["DATABASE_URL"]
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
