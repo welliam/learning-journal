a630f5c8319d81379bdac949b210f66ea73972f5
Justin Lange <well1912@gmail.com>
2016-08-30 11:07:22 -0700

    Add session factory setting to security.py

--- learning_journal/security.py
+++ learning_journal/security.py
@@ -1,6 +1,7 @@
 import os
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.session import SignedCookieSessionFactory
 from pyramid.security import Everyone, Authenticated, Allow
 
 from passlib.apps import custom_app_context as pwd_context
@@ -30,6 +31,12 @@ def check_credentials(user, password):
     return False
 
 
+def set_session_factory(config):
+    session_secret = os.environ.get('SESSION_SECRET', '~seeecret~')
+    session_factory = SignedCookieSessionFactory(session_secret)
+    config.set_session_factory(session_factory)
+
+
 def includeme(config):
     """Security-related configs."""
     auth_secret = os.environ.get('AUTH_SECRET', '~secret~')
@@ -40,3 +47,4 @@ def includeme(config):
     config.set_authorization_policy(ACLAuthorizationPolicy())
     config.set_default_permission('view')
     config.set_root_factory(Root)
+    set_session_factory(config)
