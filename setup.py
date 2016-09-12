cddba8dd405e9017dc1251e67d3d0445637d6c4f
Justin Lange <well1912@gmail.com>
2016-09-11 20:46:08 -0700

    Remove problematic (for tox?) lines from setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
-    README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
-
 requires = [
     'pyramid',
     'pyramid_jinja2',
@@ -31,7 +25,6 @@ tests_require = [
 setup(name='learning_journal',
       version='0.0',
       description='learning_journal',
-      long_description=README + '\n\n' + CHANGES,
       classifiers=[
           "Programming Language :: Python",
           "Framework :: Pyramid",
